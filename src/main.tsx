d671f44bbf2e1890172d427a7cca83ff9b62fbc8
goduer <526140067@qq.com>
2023-05-25 20:13:54 +0800

    deleted:    src/App.css 	modified:   src/App.tsx 	deleted:    src/index.css 	new file:   src/index.less 	modified:   src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import * as ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
+import './index.less'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <App />
